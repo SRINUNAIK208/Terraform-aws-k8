01cb89ae1f81b4f1d362a125e6dd544652d5a94e
SRINUNAIK208 <srinaik208@gmail>
2026-03-31 00:07:02 +0530

    k8-roboshop

--- ingress_alb/data.tf
+++ ingress_alb/data.tf
@@ -8,8 +8,8 @@ data "aws_ssm_parameter" "vpc_id" {
 
 
 
-  data "aws_ssm_parameter" "ingress_alb_sg_id" {
-    name = "/${var.project}/${var.environment}/ingress_alb_sg_id"
+  data "aws_ssm_parameter" "ingress_sg_id" {
+    name = "/${var.project}/${var.environment}/ingress_sg_id"
   }
   
    data "aws_ssm_parameter" "acm_certificate_arn" {
